0705e505748e21c1d8b75bf72a7232c86ef5ac24
Yhucee14 <uchechukwu7814@gmail.com>
2024-11-02 12:29:48 -0700

    not a good day too

--- src/react-chart/components/Sidebar.jsx
+++ src/react-chart/components/Sidebar.jsx
@@ -1,9 +1,32 @@
+import { BarChart2, Icon, ShoppingBag } from "lucide-react";
+import { useState } from "react";
+import { motion } from "framer-motion";
 
+const SIDEBAR_ITEMS = [
+  {
+    name: "Overview",
+    Icon: BarChart2,
+    color: "#6366f1",
+    path: "/overview",
+  },
+  {
+    name: "Products",
+    Icon: ShoppingBag,
+    color: "#ec4899",
+    path: "/product",
+  },
+];
 
 const Sidebar = () => {
-  return (
-    <div>Sidebar</div>
-  )
-}
+  const [isSideBarOpen, setIsSideBarOpen] = useState(true);
 
-export default Sidebar
+  return (
+    <motion.div
+      className={`relative z-10 transition-all duration-300 ease-in-out flex-shrink-0 ${
+        isSideBarOpen ? "w-64" : "w-20"
+      }`}
+    ></motion.div>
+  );
+};
+
+export default Sidebar;
