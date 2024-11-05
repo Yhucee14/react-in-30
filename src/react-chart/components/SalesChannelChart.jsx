881f8c202a050dd2e770aca0c7efc83680b2ca9f
Yhucee14 <uchechukwu7814@gmail.com>
2024-11-05 10:24:46 -0800

    started sales channel

--- src/react-chart/components/SalesChannelChart.jsx
+++ src/react-chart/components/SalesChannelChart.jsx
@@ -1,4 +1,13 @@
+import { motion } from "framer-motion";
 
+const COLORS = ['#6366f1', '#885cf6', '#ec4899', '#108981', '#f59e0b']
+
+const salesChannelData = [
+    { name: "Website", value: 45600 },
+    { name: "Mobile App", value: 36200 },
+    { name: "Marketplace", value: 29800 },
+    { name: "Social Media", value: 18700 },
+  ];
 
 const SalesChannelChart = () => {
   return (
