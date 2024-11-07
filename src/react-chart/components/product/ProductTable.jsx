0548a29150ad2a733fd297a6ce6e98cff0a584f2
Yhucee14 <uchechukwu7814@gmail.com>
2024-11-07 13:01:10 -0800

    lazy

--- src/react-chart/components/product/ProductTable.jsx
+++ src/react-chart/components/product/ProductTable.jsx
@@ -1,4 +1,11 @@
 
+const productData = [
+  { month: "Jan", sales: 4500 },
+  { month: "Feb", sales: 3200 },
+  { month: "Mar", sales: 5300 },
+  { month: "Apr", sales: 2180 },
+  { month: "May", sales: 1900 },
+];
 
 const ProductTable = () => {
   return (
